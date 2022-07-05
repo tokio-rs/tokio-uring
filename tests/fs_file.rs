d3d3554c24ea5dbf4feb5a4a5f22ab4ebdc8cc9a
Jiang Liu <gerry@linux.alibaba.com>
2022-07-05 17:27:57 +0800

    fs: implement File::readv_at() for vectored io
    Implement readv_at() for fs::File to support vectored io.
    
    Signed-off-by: Jiang Liu <gerry@linux.alibaba.com>

--- tests/fs_file.rs
+++ tests/fs_file.rs
@@ -48,6 +48,22 @@ fn basic_write() {
 }
 
 #[test]
+fn vectored_read() {
+    tokio_uring::start(async {
+        let mut tempfile = tempfile();
+        tempfile.write_all(HELLO).unwrap();
+
+        let file = File::open(tempfile.path()).await.unwrap();
+        let bufs = vec![Vec::<u8>::with_capacity(5), Vec::<u8>::with_capacity(9)];
+        let (res, bufs) = file.readv_at(bufs, 0).await;
+        let n = res.unwrap();
+
+        assert_eq!(n, HELLO.len());
+        assert_eq!(bufs[1][0], b' ');
+    });
+}
+
+#[test]
 fn cancel_read() {
     tokio_uring::start(async {
         let mut tempfile = tempfile();
