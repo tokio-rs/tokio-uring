864a860b5b24f21cbe397b0239ffaec9f299e557
Mikhail Zabaluev <mikhail.zabaluev@gmail.com>
2022-11-30 23:48:04 +0200

    buf/fixed: correct doc on FixedBuf

--- src/buf/fixed/handle.rs
+++ src/buf/fixed/handle.rs
@@ -21,13 +21,15 @@ pub(super) struct CheckedOutBuf {
 /// A unique handle to a memory buffer that can be pre-registered with
 /// the kernel for `io-uring` operations.
 ///
-/// `FixedBuf` handles can be obtained from a [`FixedBufRegistry`] collection.
+/// `FixedBuf` handles can be obtained from a collection of fixed buffers,
+/// either [`FixedBufRegistry`] or [`FixedBufPool`].
 /// For each buffer, only a single `FixedBuf` handle can be either used by the
 /// application code or owned by an I/O operation at any given time,
 /// thus avoiding data races between `io-uring` operations in flight and
 /// the application accessing buffer data.
 ///
 /// [`FixedBufRegistry`]: super::FixedBufRegistry
+/// [`FixedBufPool`]:     super::FixedBufPool
 ///
 pub struct FixedBuf {
     registry: Rc<RefCell<dyn FixedBuffers>>,
