bb5a00c5e16bfeb31bfb15530dd07e30124a31e2
Mikhail Zabaluev <mikhail.zabaluev@gmail.com>
2022-12-03 13:24:52 +0200

    `FixedBufPool`: a dynamic fixed buffer collection (#190)
    Make the internal infrastructure for fixed buffers more flexible,
    allowing `FixedBuf` handles to be backed by alternative collections.
    Add one new collection as `FixedBufPool`, enabling dynamic retrieval of
    free buffers.

--- src/buf/fixed/handle.rs
+++ src/buf/fixed/handle.rs
@@ -8,19 +8,31 @@ use std::mem::ManuallyDrop;
 use std::ops::{Deref, DerefMut};
 use std::rc::Rc;
 
+// Data to construct a `FixedBuf` handle from.
+pub(super) struct CheckedOutBuf {
+    // Pointer and size of the buffer.
+    pub iovec: iovec,
+    // Length of the initialized part.
+    pub init_len: usize,
+    // Buffer index.
+    pub index: u16,
+}
+
 /// A unique handle to a memory buffer that can be pre-registered with
 /// the kernel for `io-uring` operations.
 ///
-/// `FixedBuf` handles can be obtained from a [`FixedBufRegistry`] collection.
+/// `FixedBuf` handles can be obtained from a collection of fixed buffers,
+/// either [`FixedBufRegistry`] or [`FixedBufPool`].
 /// For each buffer, only a single `FixedBuf` handle can be either used by the
 /// application code or owned by an I/O operation at any given time,
 /// thus avoiding data races between `io-uring` operations in flight and
 /// the application accessing buffer data.
 ///
 /// [`FixedBufRegistry`]: super::FixedBufRegistry
+/// [`FixedBufPool`]:     super::FixedBufPool
 ///
 pub struct FixedBuf {
-    registry: Rc<RefCell<FixedBuffers>>,
+    registry: Rc<RefCell<dyn FixedBuffers>>,
     buf: ManuallyDrop<Vec<u8>>,
     index: u16,
 }
@@ -28,25 +40,27 @@ pub struct FixedBuf {
 impl Drop for FixedBuf {
     fn drop(&mut self) {
         let mut registry = self.registry.borrow_mut();
-        debug_assert_eq!(
-            registry.iovecs()[self.index as usize].iov_base as *const u8,
-            self.buf.as_ptr()
-        );
-        debug_assert_eq!(
-            registry.iovecs()[self.index as usize].iov_len,
-            self.buf.capacity()
-        );
-        registry.check_in(self.index as usize, self.buf.len());
+        // Safety: the length of the initialized data in the buffer has been
+        // maintained accordingly to the safety contracts on
+        // Self::new and IoBufMut.
+        unsafe {
+            registry.check_in(self.index, self.buf.len());
+        }
     }
 }
 
 impl FixedBuf {
-    pub(super) unsafe fn new(
-        registry: Rc<RefCell<FixedBuffers>>,
-        iovec: iovec,
-        init_len: usize,
-        index: u16,
-    ) -> Self {
+    // Safety: Validity constraints must apply to CheckedOutBuf members:
+    // - iovec must refer to an array allocated by Vec<u8>;
+    // - the array will not be deallocated until the buffer is checked in;
+    // - the data in the array must be initialized up to the number of bytes
+    //   given in init_len.
+    pub(super) unsafe fn new(registry: Rc<RefCell<dyn FixedBuffers>>, data: CheckedOutBuf) -> Self {
+        let CheckedOutBuf {
+            iovec,
+            init_len,
+            index,
+        } = data;
         let buf = Vec::from_raw_parts(iovec.iov_base as _, init_len, iovec.iov_len);
         FixedBuf {
             registry,
--- src/io/register_buffers.rs
+++ src/io/register_buffers.rs
@@ -5,24 +5,24 @@ use std::cell::RefCell;
 use std::io;
 use std::rc::Rc;
 
-pub(crate) fn register_buffers(buffers: &Rc<RefCell<FixedBuffers>>) -> io::Result<()> {
+pub(crate) fn register_buffers(buffers: Rc<RefCell<dyn FixedBuffers>>) -> io::Result<()> {
     CONTEXT.with(|cx| {
         cx.with_driver_mut(|driver| {
             driver
                 .uring
                 .submitter()
                 .register_buffers(buffers.borrow().iovecs())?;
-            driver.fixed_buffers = Some(Rc::clone(buffers));
+            driver.fixed_buffers = Some(buffers);
             Ok(())
         })
     })
 }
 
-pub(crate) fn unregister_buffers(buffers: &Rc<RefCell<FixedBuffers>>) -> io::Result<()> {
+pub(crate) fn unregister_buffers(buffers: Rc<RefCell<dyn FixedBuffers>>) -> io::Result<()> {
     CONTEXT.with(|cx| {
         cx.with_driver_mut(|driver| {
             if let Some(currently_registered) = &driver.fixed_buffers {
-                if Rc::ptr_eq(buffers, currently_registered) {
+                if Rc::ptr_eq(&buffers, currently_registered) {
                     driver.uring.submitter().unregister_buffers()?;
                     driver.fixed_buffers = None;
                     return Ok(());
