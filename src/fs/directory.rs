b7c18551451233d87c21fd07744dc2d7b4b7ce55
Rick Richardson <ririchardson@tesla.com>
2023-01-09 17:03:10 +0000

    enhancing fs docs

--- src/fs/directory.rs
+++ src/fs/directory.rs
@@ -4,17 +4,19 @@ use std::path::Path;
 
 /// Creates a directory on the local filesystem.
 ///
-/// Returns unit on success, and ['std::io::Error'] on failure.
-///
 /// # Errors
 ///
 /// This function will return an error in the following situations, but is not
 /// limited to just these cases:
 ///
-/// * User lacks permissions to create a directory at `path`.
+/// * User lacks permissions to create a directory at `path`
+///      * [`io::ErrorKind`] would be set to `PermissionDenied`
 /// * A parent of the given path doesn't exist.
+///      * [`io::ErrorKind`] would be set to `NotFound` or `NotADirectory`
 /// * `path` already exists.
+///      * [`io::ErrorKind`] would be set to `AlreadyExists`
 ///
+/// [`ErrorKind`]: std::io::ErrorKind
 /// # Examples
 ///
 /// ```no_run
@@ -34,7 +36,11 @@ pub async fn create_dir<P: AsRef<Path>>(path: P) -> io::Result<()> {
 
 /// Removes a directory on the local filesystem.
 ///
-/// Returns unit on success, and ['std::io::Error'] on failure.
+/// This will only remove empty directories with no children. If you want to destroy the entire
+/// contents of a directory, you may try [`remove_dir_all`] which uses the standard Tokio executor.
+/// There currently is no implementation of `remove_dir_all` in tokio-uring.
+///
+/// [`remove_dir_all`]: https://docs.rs/tokio/latest/tokio/fs/fn.remove_dir_all.html
 ///
 /// # Errors
 ///
@@ -42,9 +48,13 @@ pub async fn create_dir<P: AsRef<Path>>(path: P) -> io::Result<()> {
 /// limited to just these cases:
 ///
 /// * `path` doesn't exist.
+///      * [`io::ErrorKind`] would be set to `NotFound`
 /// * `path` isn't a directory.
+///      * [`io::ErrorKind`] would be set to `NotADirectory`
 /// * The user lacks permissions to modify/remove the directory at the provided `path`.
+///      * [`io::ErrorKind`] would be set to `PermissionDenied`
 /// * The directory isn't empty.
+///      * [`io::ErrorKind`] would be set to `DirectoryNotEmpty`
 ///
 /// # Examples
 ///
