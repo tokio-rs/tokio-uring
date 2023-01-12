f81b58cc21f8d44fcd63b1c51dfb3a20cb6f320b
MA-ETL <Mark.Adair@etlsystems.com>
2023-01-12 09:47:19 +0000

    Cleanup

--- src/io/sendmsg_zc.rs
+++ src/io/sendmsg_zc.rs
@@ -18,18 +18,6 @@ impl Op<SendMsgZc, MultiCQEFuture> {
     pub(crate) fn sendmsg_zc(fd: &SharedFd, msghdr: &libc::msghdr) -> io::Result<Self> {
         use io_uring::{opcode, types};
 
-        /*let io_slices = vec![IoSlice::new(unsafe {
-            std::slice::from_raw_parts(buf.stable_ptr(), buf.bytes_init())
-        })];
-
-        let socket_addr = Box::new(SockAddr::from(socket_addr));
-
-        let mut msghdr: Box<libc::msghdr> = Box::new(unsafe { std::mem::zeroed() });
-        msghdr.msg_iov = io_slices.as_ptr() as *mut _;
-        msghdr.msg_iovlen = io_slices.len() as _;
-        msghdr.msg_name = socket_addr.as_ptr() as *mut libc::c_void;
-        msghdr.msg_namelen = socket_addr.len();*/
-
         CONTEXT.with(|x| {
             x.handle().expect("Not in a runtime context").submit_op(
                 SendMsgZc {
