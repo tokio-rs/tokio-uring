8f02617f68a6bd261d6d78620d5e9ed2b03dc305
Chris Heller <cheller@tesla.com>
2023-01-19 12:11:58 -0500

    [io] add Ops for fallocate and statx

---
+++ src/io/fallocate.rs
@@ -0,0 +1,44 @@
+use std::io;
+
+use io_uring::{opcode, types};
+
+use crate::{
+    io::SharedFd,
+    runtime::{
+        driver::op::{Completable, CqeResult, Op},
+        CONTEXT,
+    },
+};
+
+pub(crate) struct Fallocate {
+    fd: SharedFd,
+}
+
+impl Op<Fallocate> {
+    pub(crate) fn fallocate(
+        fd: &SharedFd,
+        offset: u64,
+        len: u64,
+        flags: i32,
+    ) -> io::Result<Op<Fallocate>> {
+        CONTEXT.with(|x| {
+            x.handle().expect("not in a runtime context").submit_op(
+                Fallocate { fd: fd.clone() },
+                |fallocate| {
+                    opcode::Fallocate64::new(types::Fd(fallocate.fd.raw_fd()), len as _)
+                        .offset64(offset as _)
+                        .mode(flags)
+                        .build()
+                },
+            )
+        })
+    }
+}
+
+impl Completable for Fallocate {
+    type Output = io::Result<()>;
+
+    fn complete(self, cqe: CqeResult) -> Self::Output {
+        cqe.result.map(|_| ())
+    }
+}
