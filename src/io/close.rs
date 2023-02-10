24b274922386ff660ebe8250927cc7128948c683
Frank Rehwinkel <frankrehwinkel@gmail.com>
2023-02-10 12:25:39 -0500

    async close fixed support

--- src/io/close.rs
+++ src/io/close.rs
@@ -10,27 +10,29 @@ impl Op<Close> {
     pub(crate) fn close(fd: CommonFd) -> io::Result<Op<Close>> {
         use io_uring::{opcode, types};
 
-        match fd {
-            CommonFd::Raw(raw) => CONTEXT.with(|x| {
-                x.handle()
-                    .expect("Not in a runtime context")
-                    .submit_op(Close {}, |_close| {
-                        opcode::Close::new(types::Fd(raw)).build()
-                    })
-            }),
-            CommonFd::Fixed(_fixed) => {
-                /* TODO not yet implemented by io-uring
-                CONTEXT.with(|x| {
-                    x.handle()
-                        .expect("Not in a runtime context")
-                        .submit_op(Close { fd }, |_close| {
-                            opcode::Close_direct::new(_fixed).build()
-                        })
-                })
-                */
-                unreachable!("waiting for the ability to create a fixed descriptor for a request");
+        let (raw, fixed) = match fd {
+            CommonFd::Raw(raw) => (raw, None),
+            CommonFd::Fixed(fixed) => {
+                let fixed = match types::DestinationSlot::try_from_slot_target(fixed) {
+                    Ok(n) => n,
+                    Err(n) => {
+                        return Err(std::io::Error::new(
+                            std::io::ErrorKind::Other,
+                            format!("invalid fixed slot: {n}"),
+                        ))
+                    }
+                };
+                (0, Some(fixed))
             }
-        }
+        };
+
+        CONTEXT.with(|x| {
+            x.handle()
+                .expect("Not in a runtime context")
+                .submit_op(Close {}, |_close| {
+                    opcode::Close::new(types::Fd(raw)).file_index(fixed).build()
+                })
+        })
     }
 }
 
